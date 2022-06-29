a0852c83030d729bbb2975379824581e8120fc16
thangduongnfq <thang.duon@nfq.asia>
2022-06-29 11:40:20 +0700

    feat: add component InputOFPage

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -4,7 +4,15 @@ import { Route, Routes } from 'react-router-dom'
 import HomeLayout from '../layouts/Home'
 import AdminLayout from '../layouts/Admin'
 
-import { Login, Home, FlightList, Register, NoMatch, Admin } from '../pages'
+import {
+  Login,
+  Home,
+  FlightList,
+  Register,
+  NoMatch,
+  Admin,
+  Booking,
+} from '../pages'
 
 const RoutesApp = () => {
   return (
@@ -13,6 +21,7 @@ const RoutesApp = () => {
       <Route path="/" element={<HomeLayout />}>
         <Route path="" element={<Home />} />
         <Route path="flights" element={<FlightList />} />
+        <Route path="/booking-flights" element={<Booking />} />
 
         {/* Authentication */}
         <Route path="/login" element={<Login />} />
--- src/pages/Booking/index.jsx
+++ src/pages/Booking/index.jsx
@@ -1,8 +1,80 @@
-import { Layout } from 'antd'
+import { Button, Form, Layout } from 'antd'
+import './index.scss'
+import { FlightListBanner, InputOFPage } from '../../components'
 const { Header, Footer, Sider, Content } = Layout
-
 function FlightList() {
-  return <div className="booking-page">Booking page content</div>
+  return (
+    <div className="booking-page ">
+      <FlightListBanner />
+      <div className="booking-page__container grid wide">
+        <div className="booking-page__container__item">
+          <div className="booking-page__container__item__title">
+            <h2>Passenger information</h2>
+          </div>
+          <div className="booking-page__container__item__content">
+            <Form
+              name="basic"
+              wrapperCol={{
+                span: 22,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+            >
+              <Form.Item
+                name="username"
+                style={{ display: 'inline-block', width: '50%', margin: '0px' }}
+                rules={[
+                  { required: true, message: 'Please input your username!' },
+                ]}
+              >
+                <InputOFPage />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                style={{ display: 'inline-block', width: '50%' }}
+              >
+                <InputOFPage />
+              </Form.Item>
+              <Form.Item
+                name="username"
+                style={{ display: 'inline-block', width: '50%', margin: '0px' }}
+              >
+                <InputOFPage />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                style={{ display: 'inline-block', width: '50%' }}
+              >
+                <InputOFPage />
+              </Form.Item>
+              <Form.Item
+                name="username"
+                style={{ display: 'inline-block', width: '50%', margin: '0px' }}
+              >
+                <InputOFPage />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                style={{ display: 'inline-block', width: '50%' }}
+              >
+                <InputOFPage />
+              </Form.Item>
+              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+        <div className="booking-page__container__item"></div>
+      </div>
+    </div>
+  )
 }
 
 export default FlightList

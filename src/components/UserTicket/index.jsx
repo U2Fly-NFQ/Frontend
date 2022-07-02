09f57025c22030bb70db96e334582202786b06e1
Em Huynh Van Gioi <em.huynh@nfq.asia>
2022-07-02 16:20:14 +0700

    complete the layout for user profile page

--- src/components/UserTicket/index.jsx
+++ src/components/UserTicket/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Modal } from 'antd'
-import './ticketStyle.css'
+import { Col, Modal, Row } from 'antd'
 import './style.scss'
 
 function UserTicket({ visible, setViewTicket }) {
@@ -12,111 +11,144 @@ function UserTicket({ visible, setViewTicket }) {
       onCancel={() => setViewTicket(false)}
     >
       <div className="ticket">
-        <div id="barBig"></div>
-        <div id="mainInfo">
-          <div className="passenger-info-container">
-            <div className="passenger-info">
-              <span>Name of Passenger</span>
-              <span className="details">Sang Sáng Sủa</span>
-            </div>
-            <div className="passenger-info">
-              <span>Flight</span>
-              <span className="details">MH37*</span>
-            </div>
-            <div className="passenger-info">
-              <span>Date</span>
-              <span className="details">2/7/22</span>
-            </div>
-            <div className="passenger-info">
-              <span>Seat</span>
-              <span className="details">........</span>
-            </div>
-          </div>
-          <h1 className="destination">
-            Can Tho
+        <Row className="ticket-header">
+          <Col span={9} className="ticket-header-logo">
             <img
-              src="https://i.pinimg.com/originals/e9/0a/29/e90a299a041b7d37cdafc6eb2905e9d6.png"
-              alt="Destination"
+              src="https://www.vietnamairlines.com/~/media/Images/VNANew/Home/Logo%20Header/logo_vna-mobile.png"
+              alt="logo"
             />
-            Ha Noi
-          </h1>
-          <div className="flight-info-container">
-            {/*<div className="flight-info">*/}
-            {/*  <span>Gate</span>*/}
-            {/*  <span>D 12</span>*/}
-            {/*</div>*/}
-            <div className="flight-info">
-              <span>Boarding Time</span>
-              <span>8:00 AM IST</span>
-            </div>
-            <div className="flight-info">
-              <span>ETD</span>
-              <span>8:30 AM IST</span>
-            </div>
-            <div className="flight-info">
-              <span>ETA</span>
-              <span>10:15 AM IST</span>
-            </div>
-          </div>
-          <p className="note">Gate closes 20 minutes before departure</p>
-        </div>
-        <div id="circles">
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-          <div className="circle" />
-        </div>
-        <div id="sideInfo">
-          <img
-            src="https://www.incimages.com/uploaded_files/image/1024x576/*Barcode_32896.jpg"
-            alt="Small Bar"
-            id="barSmall"
-          />
-          <div className="details-container">
-            <div className="flight-details">
-              <div>
-                <span>Name of Passenger</span>
-                <span>Sang Sáng Sủa</span>
-              </div>
-              <div>
-                <span>Flight</span>
-                <span>MH37*</span>
-              </div>
-              <div>
-                <span>Date</span>
-                <span>2/7/22</span>
-              </div>
-              <div>
-                <span>Seat</span>
-                <span>..........</span>
-              </div>
-            </div>
-            <div className="destination-details">
-              CanTho
-              <img
-                src="https://i.pinimg.com/originals/e9/0a/29/e90a299a041b7d37cdafc6eb2905e9d6.png"
-                alt="Destination"
-              />
-              HaNoi
-            </div>
-          </div>
-        </div>
+          </Col>
+          <Col span={9} className="ticket-header-class"></Col>
+          <Col span={6} className="ticket-header-right">
+            <img
+              src="https://www.vietnamairlines.com/~/media/Images/VNANew/Home/Logo%20Header/logo_vna-mobile.png"
+              alt="logo"
+            />
+          </Col>
+        </Row>
+
+        <Row gutter={8} className="ticket-content">
+          <Col span={17} className="ticket-content-left">
+            <Row gutter={8}>
+              <Col className="ticket-content-left-barcode" span={4}>
+                <img
+                  src="https://cdn.pixabay.com/photo/2014/04/02/16/19/barcode-306926__480.png"
+                  alt="barcode"
+                />
+              </Col>
+              <Col className="ticket-content-left-info" span={20}>
+                <Row className="passenger-info">
+                  <Col span={10}>
+                    <div className="passenger-info-title">
+                      Full Name / Họ và Tên
+                    </div>
+                    <div className="passenger-info-text">Sang Sáng Sủa</div>
+                  </Col>
+                  <Col span={6}>
+                    <div className="passenger-info-title">
+                      Flight/Chuyến Bay
+                    </div>
+                    <div className="passenger-info-text">MH370</div>
+                  </Col>
+                  <Col span={4}>
+                    <div className="passenger-info-title">Date/Ngày</div>
+                    <div className="passenger-info-text">7/7/2022</div>
+                  </Col>
+                  <Col span={4}>
+                    <div className="passenger-info-title">Seat/Ghế</div>
+                    <div className="passenger-info-text">..............</div>
+                  </Col>
+                </Row>
+                <Row className="flight-info">
+                  <Col span={10} className="flight-info-from">
+                    Ho Chi Minh
+                  </Col>
+                  <Col span={4} className="flight-info-icon">
+                    <i className="fa-solid fa-plane"></i>
+                  </Col>
+                  <Col span={10} className="flight-info-to">
+                    Ha Noi
+                  </Col>
+                </Row>
+                <Row className="flight-details">
+                  <Col span={6}>
+                    <div className="flight-details-title">Gate/Cổng</div>
+                    <div className="flight-details-text">..............</div>
+                  </Col>
+                  <Col span={6}>
+                    <div className="flight-details-title">Boarding Time</div>
+                    <div className="flight-details-text">7:30 AM</div>
+                  </Col>
+                  <Col span={6}>
+                    <div className="flight-details-title">ETD/Khởi Hành</div>
+                    <div className="flight-details-text">8:30 AM</div>
+                  </Col>
+                  <Col span={6}>
+                    <div className="flight-details-title">ETA/Đến Nơi</div>
+                    <div className="flight-details-text">10:30 AM</div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={7} className="ticket-content-right">
+            <Row>
+              <Col span={24} className="barcode">
+                <img
+                  src="https://cdn.pixabay.com/photo/2014/04/02/16/19/barcode-306926__480.png"
+                  alt="barcode"
+                />
+              </Col>
+              <Col span={24} className="flight-info">
+                <Row>
+                  <Col span={10} className="flight-info-from">
+                    Ho Chi Minh
+                  </Col>
+                  <Col span={4} className="flight-info-icon">
+                    <i className="fa-solid fa-plane"></i>
+                  </Col>
+                  <Col span={10} className="flight-info-to">
+                    Ha Noi
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24} className="passenger-info">
+                <Row>
+                  <Col span={24}>
+                    <div className="passenger-info-title">
+                      Full Name / Họ và Tên
+                    </div>
+                    <div className="passenger-info-text">Sang Sáng Sủa</div>
+                  </Col>
+                  <Col span={24}>
+                    <div className="passenger-info-title">
+                      Flight/Chuyến Bay
+                    </div>
+                    <div className="passenger-info-text">MH370</div>
+                  </Col>
+                  <Col span={24}>
+                    <div className="passenger-info-title">Date/Ngày</div>
+                    <div className="passenger-info-text">7/7/2022</div>
+                  </Col>
+                  <Col span={24}>
+                    <div className="passenger-info-title">Seat/Ghế</div>
+                    <div className="passenger-info-text">..........</div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
+        <Row className="ticket-footer">
+          <Col span={18} className="ticket-footer-left">
+            Gate closes 20 minutes before departure - Cổng đóng 20 phút trước
+            khi khởi hành
+          </Col>
+          <Col span={6} className="ticket-footer-right">
+            Hotline: 19XX XXX XXX
+          </Col>
+        </Row>
       </div>
     </Modal>
   )

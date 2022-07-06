ad3baf8f85170a225aa13053f5dca2fde3172860
DucTranNFQ <duc.tran@nfq.asia>
2022-07-06 17:19:02 +0700

    feat: translate booking coupon

--- src/pages/Booking/BookingCoupon/index.jsx
+++ src/pages/Booking/BookingCoupon/index.jsx
@@ -5,16 +5,20 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getDiscountCheck } from '../../../redux/slices/bookingFlightsSlice'
 import { getDiscountForBookingAirline } from '../../../redux/selectors'
 import './index.scss'
+import { useTranslation } from 'react-i18next'
+
 export default function BookingCoupon() {
   const dispatch = useDispatch()
   const discount = useSelector(getDiscountForBookingAirline)
   const onFinish = (value) => {
     dispatch(getDiscountCheck(value))
   }
+  const { t } = useTranslation()
+
   return (
     <div className="booking-coupon">
       <div className="booking-page__container__item__title">
-        <h3>Coupon code</h3>
+        <h3>{t('flight-booking-page.Coupon code')}</h3>
       </div>
       <div className="booking-coupon__container">
         {discount.percent != 0 && <i className="fa-solid fa-circle-check"></i>}
@@ -33,12 +37,12 @@ export default function BookingCoupon() {
             <input
               name="idDiscount"
               className="form-control"
-              placeholder="input your discount number"
+              placeholder={t('flight-booking-page.input your discount number')}
             />
           </Form.Item>
           <Form.Item style={{ marginTop: '20px' }}>
             <ButtonOfPage
-              title={'Apply voucher'}
+              title={t('flight-booking-page.Apply voucher')}
               warning={discount.percent != 0 ? true : false}
             />
           </Form.Item>

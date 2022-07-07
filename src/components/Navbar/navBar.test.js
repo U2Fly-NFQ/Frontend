4b6ab088da41257584348928a0eba8c48934570b
zevik7 <zevik2807@gmail.com>
2022-07-07 22:45:08 +0700

    fix: update snapshot

--- src/components/Navbar/navBar.test.js
+++ src/components/Navbar/navBar.test.js
@@ -1,4 +1,6 @@
 import renderer from 'react-test-renderer'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '../../translations'
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './'
@@ -7,9 +9,11 @@ describe('Navigation bar test', () => {
   it('renders correctly', () => {
     const tree = renderer
       .create(
-        <Router>
-          <Home />
-        </Router>
+        <I18nextProvider i18n={i18n}>
+          <Router>
+            <Home />
+          </Router>
+        </I18nextProvider>
       )
       .toJSON()
     expect(tree).toMatchSnapshot()
